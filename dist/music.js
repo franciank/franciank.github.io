4dc95a992a0fb5e4749be937a4a02d41b99d2535
franciank <franciank@163.com>
2020-01-02 19:28:26 +0800

    Site updated: 2020-01-02 19:28:24

---
+++ dist/music.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "いい日だったね",
+        artist: 'Depapepe',
+        url: 'http://sc1.111ttt.cn/2015/1/06/27/99271234248.mp3',
+
+      },
+
+    ]
+});
